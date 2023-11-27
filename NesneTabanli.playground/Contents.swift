1b3e657e209861c824b8a85f4f507d76710db0bb
SAMET ŞEN <96656977+msametsen@users.noreply.github.com>
2023-11-27 13:41:52 +0300

    Add files via upload

--- NesneTabanli.playground/Contents.swift
+++ NesneTabanli.playground/Contents.swift
@@ -82,3 +82,66 @@ bmw.bilgiAl()
 //ford.bilgiAl()
 //ford.calistir()
 //ford.bilgiAl()
+
+
+
+//*******************************************3.GÜN*******************************************************************
+
+//Geri dönüş değeri olan(retunr) lü fonkiyonlarda mutlaka -> yapılıp sonrasında türünü(string,int vs.) belirtilmeli
+
+class Fonksiyonlar {
+    //Geri dönüş değeri olmayan(void)
+    func selamla1() {
+        let sonuc = "Merhaba Ahmet"
+        print(sonuc)
+    }
+    
+    
+    //Geri dönüş değeri olan(return)
+    func selamla2() -> String {
+        let sonuc = "Merhaba Ahmet"
+        return sonuc
+    }
+    
+    
+    
+    //parametre
+    func selamla3(isim:String) {
+        let sonuc = "Merhaba \(isim)"
+        print(sonuc)
+    }
+    
+    
+    func toplama(sayi1:Int,sayi2:Int) -> Int {
+        let toplam = sayi1 + sayi2
+        return toplam
+    }
+    
+    
+    
+    //Overloading-Bu sayede aynı isimli func tekrar tekrar kullanmamızı sağlar.Ama en az bir değerin türü değişmeli yada yeni bir parametre eklemelisin
+    func carpma(sayi1:Int,sayi2:Int) {
+       print("Çarpma : \(sayi1 * sayi2)")
+    }
+    
+    func carpma(sayi1:Int,sayi2:Int,isim:String) {
+       print("Çarpma : \(sayi1 * sayi2) - İşlemi Yapan: \(isim)")
+    }
+    
+    
+}
+
+let f = Fonksiyonlar()
+f.selamla1()
+
+let gelenSonuc = f.selamla2()
+print("Gelen Sonuç : \(gelenSonuc)")
+
+f.selamla3(isim: "Samet")
+
+let gelenToplam = f.toplama(sayi1: 10, sayi2: 20)
+print("Gelen Toplam : \(gelenToplam)")
+
+f.carpma(sayi1: 10, sayi2: 25)
+f.carpma(sayi1: 3, sayi2: 5, isim: "Samet")
+
